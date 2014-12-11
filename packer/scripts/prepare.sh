258c2c1f2895043529444db6ed2c76ead7f6b59f
Marius Sturm <marius@torch.sh>
2014-12-11 16:17:26 +0100

    improve vmware support

--- packer/scripts/prepare.sh
+++ packer/scripts/prepare.sh
@@ -5,3 +5,5 @@ echo %ubuntu ALL=NOPASSWD:ALL > /etc/sudoers.d/ubuntu
 chmod 0440 /etc/sudoers.d/ubuntu
 # Setup sudo to allow no-password sudo for "sudo"
 usermod -a -G sudo ubuntu
+# Update package cache
+apt-get update
