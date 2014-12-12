1da43d953e2f1ed0329e66580aefa5037baeb843
Marius Sturm <marius@torch.sh>
2014-12-12 10:38:42 +0100

    bump to 0.92.1

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget -O /tmp/graylog2.deb https://packages.graylog2.org/releases/graylog2-omnibus/ubuntu/graylog2_0.91.3-1_amd64.deb
+apt-get install -y curl
+wget -O /tmp/graylog2.deb https://packages.graylog2.org/releases/graylog2-omnibus/ubuntu/graylog2_0.92.1-1_amd64.deb
 dpkg -i /tmp/graylog2.deb
 rm /tmp/graylog2.deb
