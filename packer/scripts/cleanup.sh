318c76ec2373e7bf344718ea2597736c79c2e648
Marius Sturm <marius@torch.sh>
2014-12-17 09:57:55 +0100

    cleanup temp folders

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -7,5 +7,8 @@ rm /var/lib/dhcp/*
 echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
 
+apt-get clean
+rm -rf /tmp/* /var/tmp/*
+
 dd if=/dev/zero of=/EMPTY bs=1M
 rm /EMPTY
