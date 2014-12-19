2d104364b372aa11a03b879a020862f8ced5e258
Marius Sturm <marius.sturm@googlemail.com>
2014-12-19 21:35:35 +0100

    cleanup apt update

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+apt-get update
 apt-get install -y curl
-wget -O /tmp/graylog2.deb https://packages.graylog2.org/releases/graylog2-omnibus/ubuntu/graylog2_0.92.1-1_amd64.deb
+wget -O /tmp/graylog2.deb https://packages.graylog2.org/releases/graylog2-omnibus/ubuntu/graylog2_latest.deb
 dpkg -i /tmp/graylog2.deb
 rm /tmp/graylog2.deb
--- packer/scripts/install_vmware.sh
+++ packer/scripts/install_vmware.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-apt-get update
 apt-get install -y open-vm-tools
