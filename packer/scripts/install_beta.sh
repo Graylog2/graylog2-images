c4ee2e61b39b3a88de4255f34613fb9ff68aa380
Marius Sturm <marius.sturm@googlemail.com>
2015-01-15 20:25:32 +0100

    add beta ova

---
+++ packer/scripts/install_beta.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+apt-get update
+apt-get install -y curl wget rsync vim man sudo avahi-autoipd
+apt-get install -y tzdata
+apt-get install -y ntp
+apt-get install -y ntpdate
+wget -O /tmp/graylog2.deb https://packages.graylog2.org/releases/graylog2-omnibus/ubuntu/graylog2_beta.deb
+dpkg -i /tmp/graylog2.deb
+rm /tmp/graylog2.deb
