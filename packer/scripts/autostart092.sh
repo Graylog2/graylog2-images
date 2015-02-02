bc7f89246a25a006ac0d16ce5e2e5c2ddec805f4
Marius Sturm <marius@torch.sh>
2015-02-02 15:59:43 +0100

    autostart for 0.92 branch

---
+++ packer/scripts/autostart092.sh
@@ -0,0 +1,11 @@
+cat > /etc/rc.local <<'EOF'
+#!/bin/sh -e
+
+IP=$(hostname -I)
+echo "Open http://$IP in your browser to access Graylog2. Default username/password: 'admin'" > /etc/issue
+
+if [ ! -d "/etc/graylog2" ]; then
+        /usr/bin/graylog2-ctl reconfigure
+fi
+exit 0
+EOF
