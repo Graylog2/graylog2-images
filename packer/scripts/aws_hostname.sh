98b08fd2fe5f4ba455b42a23a21c9f63a29f6083
Marius Sturm <marius@graylog.com>
2015-02-19 17:28:18 +0100

    set proper hostname

---
+++ packer/scripts/aws_hostname.sh
@@ -0,0 +1,19 @@
+echo 'graylog' > /etc/hostname
+cat > /etc/network/if-up.d/update_hosts <<'EOF'
+#!/bin/bash
+
+#/etc/network/if-up.d/update_hosts
+set -e
+
+#Variable IFACE is setup by Ubuntu network init scripts to whichever interface changed status.
+[ "$IFACE" == "eth0" ] || exit
+
+myname=`cat /etc/hostname`
+shortname=`cat /etc/hostname | cut -d "." -f1`
+hostsfile="/etc/hosts"
+#Knock out line with "old" IP
+sed -i '/ '$myname'/ d' $hostsfile
+ipaddr=$(ifconfig eth0 | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+echo "$ipaddr $myname $shortname" >> $hostsfile
+EOF
+chmod +x /etc/network/if-up.d/update_hosts
