c27aaa51321cb9b3e62994ec267f5583536b1085
Marius Sturm <marius.sturm@googlemail.com>
2015-02-23 20:13:40 +0100

    add seperate script for setting up shell environment

--- packer/scripts/aws_hostname.sh
+++ packer/scripts/aws_hostname.sh
@@ -18,8 +18,3 @@ echo "$ipaddr $myname $shortname" >> $hostsfile
 EOF
 chmod +x /etc/network/if-up.d/update_hosts
 sed -i 's/^preserve_hostname.*/preserve_hostname: true/' /etc/cloud/cloud.cfg
-
-cat > /etc/environment <<'EOF'
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
-LC_ALL="en_US.UTF-8"
-EOF
---
+++ packer/scripts/shell_environment.sh
@@ -0,0 +1,5 @@
+cat > /etc/environment <<'EOF'
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+LC_ALL="en_US.UTF-8"
+JAVA_HOME="/opt/graylog/embedded/jre"
+EOF
