5810376c6779e0fd1c2aa707bada3b729c7c3157
Marius Sturm <marius.sturm@googlemail.com>
2015-02-25 12:10:01 +0100

    cleanup packer temp. keys

---
+++ packer/scripts/aws_cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+# in case there are changes for grub
+update-grub
+
+# Removing leftover leases and persistent rules
+echo "cleaning up dhcp leases"
+rm -f /var/lib/dhcp/*
+
+apt-get autoremove
+apt-get autoclean
+apt-get clean
+
+rm -rf /tmp/* /var/tmp/*
+rm -f /var/log/wtmp /var/log/btmp
+rm -f /root/.ssh/authorized_keys
+rm -f /home/ubuntu/.ssh/authorized_keys
+history -c
