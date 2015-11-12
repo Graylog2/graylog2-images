f795ce8f21da308763a940ca19b99e00b6142c44
Marius Sturm <marius@graylog.com>
2015-11-12 14:01:42 +0100

    add installation_source flag

---
+++ packer/scripts/installation_source_ami.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -x
+
+echo 'GRAYLOG_INSTALLATION_SOURCE="ami"' > /opt/graylog/embedded/share/graylog/installation-source.sh
---
+++ packer/scripts/installation_source_qcow.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -x
+
+echo 'GRAYLOG_INSTALLATION_SOURCE="qcow"' > /opt/graylog/embedded/share/graylog/installation-source.sh
