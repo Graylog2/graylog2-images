98488d04235a19bbd90ec570fe9f4adc0163d87d
Marius Sturm <marius@graylog.com>
2015-11-18 20:28:18 +0100

    use snapshot for beta build

--- packer/scripts/install_beta_ova.sh
+++ packer/scripts/install_beta_ova.sh
@@ -7,7 +7,8 @@ apt-get install -y curl wget rsync vim man sudo avahi-autoipd
 apt-get install -y tzdata
 apt-get install -y ntp
 apt-get install -y ntpdate
-wget -O /tmp/graylog.deb https://packages.graylog2.org/releases/graylog2-omnibus/ubuntu/graylog_beta.deb
+#wget -O /tmp/graylog.deb https://packages.graylog2.org/releases/graylog2-omnibus/ubuntu/graylog_beta.deb
+wget -O /tmp/graylog.deb https://packages.graylog2.org/releases/graylog2-omnibus/ubuntu/graylog_1.3.0-SNAPSHOT-20151118121016-1_amd64.deb
 dpkg -i /tmp/graylog.deb
 rm /tmp/graylog.deb
 echo 'GRAYLOG_INSTALLATION_SOURCE="ova"' > /opt/graylog/embedded/share/graylog/installation-source.sh
