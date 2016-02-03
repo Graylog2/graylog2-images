b490de092775fb76a64e02bd9049a178852924bf
Marius Sturm <marius@graylog.com>
2016-02-03 12:35:31 +0100

    graylog2 -> graylog

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -7,6 +7,6 @@ apt-get install -y curl wget rsync vim man sudo avahi-autoipd
 apt-get install -y tzdata
 apt-get install -y ntp
 apt-get install -y ntpdate
-wget -O /tmp/graylog.deb https://packages.graylog2.org/releases/graylog2-omnibus/ubuntu/graylog_latest.deb
+wget -O /tmp/graylog.deb https://packages.graylog2.org/releases/graylog-omnibus/ubuntu/graylog_latest.deb
 dpkg -i /tmp/graylog.deb
 rm /tmp/graylog.deb
--- packer/scripts/install_ova.sh
+++ packer/scripts/install_ova.sh
@@ -7,7 +7,7 @@ apt-get install -y curl wget rsync vim man sudo avahi-autoipd
 apt-get install -y tzdata
 apt-get install -y ntp
 apt-get install -y ntpdate
-wget -O /tmp/graylog.deb https://packages.graylog2.org/releases/graylog2-omnibus/ubuntu/graylog_latest.deb
+wget -O /tmp/graylog.deb https://packages.graylog2.org/releases/graylog-omnibus/ubuntu/graylog_latest.deb
 dpkg -i /tmp/graylog.deb
 rm /tmp/graylog.deb
 echo 'GRAYLOG_INSTALLATION_SOURCE="ova"' > /opt/graylog/embedded/share/graylog/installation-source.sh
