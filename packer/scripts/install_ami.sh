b4872990f180e7dd3ad7c7fcf882b568c20a709b
Marius Sturm <marius@graylog.com>
2016-03-14 18:09:48 +0100

    fix variable name

--- packer/scripts/install_ami.sh
+++ packer/scripts/install_ami.sh
@@ -16,7 +16,7 @@ apt-get install -y curl wget rsync vim man sudo avahi-autoipd
 apt-get install -y tzdata
 apt-get install -y ntp
 apt-get install -y ntpdate
-wget -O /tmp/graylog.deb https://packages.graylog2.org/releases/graylog-omnibus/ubuntu/graylog_${GIT_TAG_NAME}_amd64.deb
+wget -O /tmp/graylog.deb https://packages.graylog2.org/releases/graylog-omnibus/ubuntu/graylog_${PACKAGE_VERSION}_amd64.deb
 dpkg -i /tmp/graylog.deb
 rm /tmp/graylog.deb
 echo 'GRAYLOG_INSTALLATION_SOURCE="ami"' > /opt/graylog/embedded/share/graylog/installation-source.sh
