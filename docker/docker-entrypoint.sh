d0a8242f18fffd462fb4a84991e452a39683e209
Pascal Friederich <paukul@gmail.com>
2016-06-09 23:09:28 +0200

    fixes log4j file location configuration
    see https://logging.apache.org/log4j/2.0/faq.html#config_location

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -20,7 +20,7 @@ if [ "$1" = 'graylog' -a "$(id -u)" = '0' ]; then
   # Start Graylog server
   set -- gosu graylog "$JAVA_HOME/bin/java" $GRAYLOG_SERVER_JAVA_OPTS \
       -jar \
-      -Dlog4j.configuration=file:///usr/share/graylog/data/config/log4j2.xml \
+      -Dlog4j.configurationFile=/usr/share/graylog/data/config/log4j2.xml \
       -Djava.library.path=/usr/share/graylog/lib/sigar/ \
       -Dgraylog2.installation_source=docker /usr/share/graylog/graylog.jar \
       server \
