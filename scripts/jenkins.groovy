8d03cb612bb45a03329b3043b3769bd01637e812
Donald Morton <donald.morton@graylog.com>
2020-06-12 14:59:52 -0500

    Add branch parameter description

--- scripts/jenkins.groovy
+++ scripts/jenkins.groovy
@@ -19,7 +19,7 @@ pipeline
    parameters
    {
        string(name: 'Graylog_Version', defaultValue: '', description: '2.x: 2.5.0-beta.1-1 (the git tag in graylog2-images) | 3.x: 3.0.0-4.alpha.4 (the deb package version)')
-       gitParameter branchFilter: 'origin/(.*)', defaultValue: 'jenkins', name: 'BRANCH', type: 'PT_BRANCH', sortMode: 'DESCENDING_SMART'
+       gitParameter branchFilter: 'origin/(.*)', defaultValue: 'jenkins', name: 'BRANCH', type: 'PT_BRANCH', sortMode: 'DESCENDING_SMART', description: 'The branch name that should be used for the build.'
        extendedChoice(name: 'Image_Type',
                       type: 'PT_CHECKBOX',
                       multiSelectDelimiter: " ", // this only defines delimiter used in the output string value, not used for parsing value input, which must be comma-separated!
